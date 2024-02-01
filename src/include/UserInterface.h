d4076892a2f5e282f1e7381e5e43b67ba4990a04
Reza Arjmandi <arjmandi.re@gmail.com>
2024-02-01 08:19:56 -0500

    Fix telnet issue

--- src/include/UserInterface.h
+++ src/include/UserInterface.h
@@ -30,27 +30,31 @@ class UserInterface: public std::enable_shared_from_this<UserInterface> {
   }
 
   template<typename HandlerType>
-  void doRead(char* data, int maxLength, HandlerType onRead) {
-    socket.async_read_some(boost::asio::buffer(data, maxLength), onRead);
-  }
-
-  template<typename HandlerType>
   void doWrite(const std::string& msg, HandlerType onWrite) {
     boost::asio::async_write(socket, boost::asio::buffer(msg.data(), msg.size()), onWrite);
   }
 
  private:
+
+   void doRead(std::function<void(std::string)> onRead) {
+    boost::asio::async_read_until(socket, buff, '\n', [onRead, this](boost::system::error_code ec, std::size_t length) {
+      if (ec) return;
+      std::istream is(&buff);
+      std::string s;
+      is >> s;
+      onRead(s);
+    });
+  }
+
   void readNumber(std::function<void(int, bool&)> handler) {
-    std::memset(buff, '\0', buffLength);
-    doRead(buff, buffLength,
-      [this, handler](boost::system::error_code ec, std::size_t length) {
-        if (!ec) onReadNumber(handler);
+    doRead(
+      [this, handler](std::string result) {
+        onReadNumber(result, handler);
       });
   }
 
-  void onReadNumber(std::function<void(int, bool&)> handler) {
+  void onReadNumber(std::string& buffStr, std::function<void(int, bool&)> handler) {
     try {
-      std::string buffStr(buff);
       auto number = std::stoi(buffStr);
       if (number > 0) {
         bool reTake = false;
@@ -62,16 +66,14 @@ class UserInterface: public std::enable_shared_from_this<UserInterface> {
   }
 
   void readString(std::function<void(std::string&)> handler) {
-    std::memset(buff, '\0', buffLength);
-    doRead(buff, buffLength,
-      [this, handler](boost::system::error_code ec, std::size_t length) {
-        if (!ec) onReadString(handler);
+    doRead(
+      [this, handler](std::string result) {
+        onReadString(result, handler);
       });
   }
 
-  void onReadString(std::function<void(std::string&)> handler) {
+  void onReadString(std::string& buffStr, std::function<void(std::string&)> handler) {
     try {
-      std::string buffStr(buff);
       trim(buffStr);
       if (!buffStr.empty()) {
         handler(buffStr);
@@ -82,6 +84,5 @@ class UserInterface: public std::enable_shared_from_this<UserInterface> {
   }
 
   tcp::socket socket;
-  enum { buffLength = 1024 };
-  char buff[buffLength];
+  boost::asio::streambuf buff;
 };
